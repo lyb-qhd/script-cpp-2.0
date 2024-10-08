d2586dfaa1743e454ed226c51b8824797705fc21
lyb-qhd <linyubin8768@163.com>
2024-10-08 19:19:04 +0800

    10081918

---
+++ cr(out).bat
@@ -0,0 +1,2 @@
+@echo off
+util/cr
---
+++ head.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+#define u using
+#define us unsigned
+u uchar=us char;
+u ushort=us short;
+u uint=us int;
+u int64=long long;
+u uint64=us long long;
+u f128=long double;
+u str=std::string;
+#undef u
+#undef us
+int main() {
+	#include "../main.cpp"
+	return 0;
+}
+
+
+
